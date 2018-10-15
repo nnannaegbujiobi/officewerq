98153812178273e0dbf5acf3e07fb640a0c90876
nnannaegbujiobi <nnanna@newvybe.com>
2018-10-15 14:18:34 -0500

    associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+namespace :api do 
+#users restful routes 
+
+
+post "users" => "users#create"
+
+get "/users/:id" => "users#show"
+
+
+
+patch "/users/:id/" => "users#update"
+
+#workout restful routes
+
+get "workouts/:id" => "workouts#show"
+
+#user_workout
+
+get "/user_workout/:id" => "user_workout#show"
+
+
+end
+
 end
